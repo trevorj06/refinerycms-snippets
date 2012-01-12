ded938184698abff0b06db47643800dfe982ff54
Trevor Jones <tjones@insightcards.com>
2012-01-12 09:01:16 -0600

    Fixed syntax error for joins

--- lib/extensions/page_extensions.rb
+++ lib/extensions/page_extensions.rb
@@ -8,7 +8,7 @@ module Extensions
         scope :for_snippet, lambda{ |snippet|
           raise RuntimeError.new("Couldn't find Pages for a nil Snippet") if snippet.blank?
           {
-            :joins => {:parts, :snippets},
+            :joins => [:parts, :snippets],
             :conditions => {:snippets_page_parts => {:snippet_id => snippet.id}}
           }
         }
